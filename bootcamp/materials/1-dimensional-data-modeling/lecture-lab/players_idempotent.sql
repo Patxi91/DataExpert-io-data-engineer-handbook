12148ed010b5826f28eb9ed38566dc84df1a12ac
Patxi Oyaga <oyaga888@gmail.com>
2025-01-09 14:53:10 +0100

    little pipeline

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_idempotent.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_idempotent.sql
@@ -116,3 +116,43 @@ SELECT
 	COALESCE(t.season, y.current_season + 1) as current_season
 FROM today t FULL OUTER JOIN yesterday y
 	ON t.player_name = y.player_name
+-- Turn the previous command into a little pipeline
+DELETE FROM players; -- Idempotent
+INSERT INTO players
+WITH yesterday AS(
+	SELECT * FROM players
+	WHERE current_season = 1995 -- Since SELECT MIN(season) FROM public.player_seasons = 1996
+),
+	today AS (
+		SELECT * FROM public.player_seasons
+		WHERE season = 1996
+	)
+SELECT 
+    COALESCE(t.player_name, y.player_name) AS player_name,
+    COALESCE(t.height, y.height) AS height,
+    COALESCE(t.college, y.college) AS college,
+	COALESCE(t.country, y.country) AS country,
+    COALESCE(t.draft_year, y.draft_year) AS draft_year,
+    COALESCE(t.draft_round, y.draft_round) AS draft_round,
+    COALESCE(t.draft_number, y.draft_number) AS draft_number,
+	CASE 
+		WHEN y.season_stats IS NULL -- Create initial array w/ 1 value if null
+			THEN ARRAY[ROW(
+							t.season,
+							t.gp,
+							t.pts,
+							t.ast,
+							t.reb
+							)::season_stats]
+		WHEN t.season IS NOT NULL THEN y.season_stats || ARRAY[ROW( -- Create new value if Today not null
+							t.season,
+							t.gp,
+							t.pts,
+							t.ast,
+							t.reb
+							)::season_stats]
+	ELSE y.season_stats -- Carry history forward otherwise (retired player)
+	END as season_stats,
+	COALESCE(t.season, y.current_season + 1) as current_season
+FROM today t FULL OUTER JOIN yesterday y
+	ON t.player_name = y.player_name
