72ba403adeb3d3856566571a76109499ca3d90b9
Patxi Oyaga <oyaga888@gmail.com>
2025-01-09 15:11:33 +0100

    Load Year by Year 2000-2001

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_idempotent.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/players_idempotent.sql
@@ -156,15 +156,15 @@ SELECT
 	COALESCE(t.season, y.current_season + 1) as current_season
 FROM today t FULL OUTER JOIN yesterday y
 	ON t.player_name = y.player_name
--- Load Year by Year 1999
+-- Load Year by Year 2000-2001
 INSERT INTO players
 WITH yesterday AS(
 	SELECT * FROM players
-	WHERE current_season = 1999 -- Since SELECT MIN(season) FROM public.player_seasons = 1996
+	WHERE current_season = 2000 -- Since SELECT MIN(season) FROM public.player_seasons = 1996
 ),
 	today AS (
 		SELECT * FROM public.player_seasons
-		WHERE season = 2000
+		WHERE season = 2001
 	)
 SELECT 
     COALESCE(t.player_name, y.player_name) AS player_name,
@@ -195,4 +195,4 @@ SELECT
 	COALESCE(t.season, y.current_season + 1) as current_season
 FROM today t FULL OUTER JOIN yesterday y
 	ON t.player_name = y.player_name
-SELECT * FROM players WHERE current_season = 2000
+SELECT * FROM players WHERE current_season = 2001
