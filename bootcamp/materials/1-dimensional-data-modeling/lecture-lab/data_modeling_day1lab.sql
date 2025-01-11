eb04b9764b299342a1f0f2cdc2482f795fb3b31a
Patxi Oyaga <oyaga888@gmail.com>
2025-01-11 18:55:53 +0100

    Insert into the SCD Table

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day1lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day1lab.sql
@@ -194,3 +194,91 @@ SELECT
 FROM with_streaks
 GROUP BY player_name, streak_identifier, is_active, scoring_class
 ORDER BY player_name
+-- Filter down SCD Table to 2021, later to use 2022 in the incremental build
+WITH
+	with_previous AS(
+		SELECT 
+			player_name,
+			current_season,
+			scoring_class,
+			is_active,
+			LAG(scoring_class, 1) OVER(PARTITION BY player_name ORDER BY current_season) as previous_scoring_class,
+			LAG(is_active, 1) OVER(PARTITION BY player_name ORDER BY current_season) as previous_is_active
+		FROM players
+		WHERE current_season <= 2021		
+	),
+	with_indicators AS( -- combined OR change indicator
+		SELECT *,
+				CASE
+					WHEN scoring_class <> previous_scoring_class THEN 1
+					WHEN is_active <> previous_is_active THEN 1 
+					ELSE 0 
+				END AS change_indicator
+		FROM with_previous
+	),
+	with_streaks AS(-- streak identifier
+		SELECT *,
+				SUM(change_indicator) OVER(PARTITION BY player_name ORDER BY current_season) AS streak_identifier
+		FROM with_indicators
+	)
+SELECT 
+	player_name,
+	scoring_class,
+	is_active,
+	MIN(current_season) AS start_season,
+	MAX(current_season) AS end_season,
+	2021 AS current_season -- hardcoded
+FROM with_streaks
+GROUP BY player_name, streak_identifier, is_active, scoring_class
+ORDER BY player_name, streak_identifier
+-- Modify PKs for players_scd (start_season & player_name) and Insert into the SCD Table
+DROP TABLE players_scd
+CREATE TABLE IF NOT EXISTS players_scd(
+	player_name TEXT,
+	scoring_class scoring_class,
+	is_active BOOLEAN,
+	start_season INTEGER,
+	end_season INTEGER,
+	current_season INTEGER,
+	PRIMARY KEY(player_name, start_season)
+)
+
+INSERT INTO players_scd
+WITH
+	with_previous AS(
+		SELECT 
+			player_name,
+			current_season,
+			scoring_class,
+			is_active,
+			LAG(scoring_class, 1) OVER(PARTITION BY player_name ORDER BY current_season) as previous_scoring_class,
+			LAG(is_active, 1) OVER(PARTITION BY player_name ORDER BY current_season) as previous_is_active
+		FROM players
+		WHERE current_season <= 2021		
+	),
+	with_indicators AS( -- combined OR change indicator
+		SELECT *,
+				CASE
+					WHEN scoring_class <> previous_scoring_class THEN 1
+					WHEN is_active <> previous_is_active THEN 1 
+					ELSE 0 
+				END AS change_indicator
+		FROM with_previous
+	),
+	with_streaks AS(-- streak identifier
+		SELECT *,
+				SUM(change_indicator) OVER(PARTITION BY player_name ORDER BY current_season) AS streak_identifier
+		FROM with_indicators
+	)
+SELECT 
+	player_name,
+	scoring_class,
+	is_active,
+	MIN(current_season) AS start_season,
+	MAX(current_season) AS end_season,
+	2021 AS current_season -- hardcoded
+FROM with_streaks
+GROUP BY player_name, streak_identifier, is_active, scoring_class
+ORDER BY player_name, streak_identifier
+
+SELECT * FROM players_scd
