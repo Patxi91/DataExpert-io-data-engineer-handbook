cf1ec26f58acd43d36afea3cdca62b369762cce7
Patxi Oyaga <oyaga888@gmail.com>
2025-01-11 20:21:35 +0100

    process compacted only the 2021&2022 records

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day1lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day1lab.sql
@@ -303,7 +303,7 @@ SELECT 	ts.player_name,
 		ts.scoring_class, ts.is_active,
 		ls.scoring_class, ls.is_active
 FROM this_season_data ts
-	LEFT JOIN last_season_scd ls
+LEFT JOIN last_season_scd ls
 	ON ls.player_name = ts.player_name
 -- Unchanged records: Increase by 1 for the records that don't change
 WITH 
@@ -328,9 +328,103 @@ WITH
 				ls.start_season,
 				ts.current_season as end_season
 		FROM this_season_data ts
-			LEFT JOIN last_season_scd ls
+		JOIN last_season_scd ls
 			ON ls.player_name = ts.player_name
 		WHERE ts.scoring_class = ls.scoring_class
 		AND ts.is_active = ls.is_active	
 	)
 SELECT * FROM unchanged_records
+-- Changed records: We have 2 records (1 for the change & 1 for the just finished closed record). We process compacted only the 2021&2022 records (20 times less data)
+DO $$ -- Check and create the type scd_type if it doesn't exist
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'scd_type') THEN
+        CREATE TYPE scd_type AS(
+								scoring_class scoring_class,
+								is_active BOOLEAN,
+								start_season INTEGER,
+								end_season INTEGER
+								);
+    END IF;
+END $$;
+
+WITH 
+	last_season_scd AS(
+		SELECT * FROM players_scd
+		WHERE current_season = 2021
+		AND end_season = 2021
+	),
+	historical_scd AS(
+		SELECT
+				player_name,
+				scoring_class,
+				is_active,
+				start_season,
+				end_season				
+		FROM players_scd
+		WHERE current_season = 2021
+		AND end_season < 2021 -- less than
+	),
+	this_season_data AS(
+		SELECT * FROM players
+		WHERE current_season = 2022
+	),
+	unchanged_records AS(
+		SELECT 	ts.player_name,
+				ts.scoring_class, 
+				ts.is_active,
+				ls.start_season,
+				ts.current_season as end_season
+		FROM this_season_data ts
+		JOIN last_season_scd ls
+			ON ls.player_name = ts.player_name
+		WHERE ts.scoring_class = ls.scoring_class
+		AND ts.is_active = ls.is_active	
+	),
+	changed_records AS ( -- Changed records
+		SELECT 	ts.player_name,
+				UNNEST(ARRAY[
+					ROW(
+						ls.scoring_class,
+						ls.is_active,
+						ls.start_season,
+						ls.end_season						
+					)::scd_type,
+					ROW(
+						ts.scoring_class,
+						ts.is_active,
+						ts.current_season,
+						ts.current_season						
+					)::scd_type
+				]) AS records
+		FROM this_season_data ts
+		LEFT JOIN last_season_scd ls
+			ON ls.player_name = ts.player_name
+		WHERE (ts.scoring_class <> ls.scoring_class
+		OR ts.is_active <> ls.is_active)
+		OR ls.player_name IS NULL
+	),
+	unnested_changed_records AS(
+		SELECT 	player_name,
+				(records::scd_type).scoring_class,
+				(records::scd_type).is_active,
+				(records::scd_type).start_season,
+				(records::scd_type).end_season
+		FROM changed_records
+	),
+	new_records AS(
+		SELECT
+				ts.player_name,
+				ts.scoring_class,
+				ts.is_active,
+				ts.current_season AS start_season,
+				ts.current_season AS end_season
+		FROM this_season_data ts
+		LEFT JOIN last_season_scd ls
+			ON ls.player_name = ts.player_name
+		WHERE ls.player_name IS NULL
+	)
+SELECT * FROM historical_scd
+UNION ALL
+SELECT * FROM unnested_changed_records
+UNION ALL
+SELECT * FROM new_records
