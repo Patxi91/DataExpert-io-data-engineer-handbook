ee7d642b4515c7b089eca4fec4ce5e1d0a692ed4
Patxi Oyaga <oyaga888@gmail.com>
2025-01-12 18:10:22 +0100

    LAB 3 comment

---
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
@@ -0,0 +1,10 @@
+-------------------------------------------------
+-- LAB 3: XXX --
+-------------------------------------------------
+/*
+Enumerations and Subpartitions pattern usecases:
+- Whenever you have tons of sources mapping to a shared schema
+    - Unit Economics (fees, coupons, credits, insurance, infrastructure cost, taxes, etc)
+    - Infrastructure Graph (applications, DBs, servers, code bases, CI/CD jobs, etc)
+    - Family of Apps (oculus, instagram, facebook, messenger, whastapp, threads, etc)
+*/
