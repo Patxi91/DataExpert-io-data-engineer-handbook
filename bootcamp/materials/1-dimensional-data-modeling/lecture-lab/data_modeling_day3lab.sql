afec37a5b6c10c1c2a2d6bead2a42aec58b24754
Patxi Oyaga <oyaga888@gmail.com>
2025-01-12 18:55:54 +0100

    update lab3 comments

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
@@ -1,5 +1,5 @@
 -------------------------------------------------
--- LAB 3: XXX --
+--   LAB 3: Graph DBs & Additive Dimensions    --
 -------------------------------------------------
 /*
 Enumerations and Subpartitions pattern usecases:
@@ -7,4 +7,22 @@ Enumerations and Subpartitions pattern usecases:
     - Unit Economics (fees, coupons, credits, insurance, infrastructure cost, taxes, etc)
     - Infrastructure Graph (applications, DBs, servers, code bases, CI/CD jobs, etc)
     - Family of Apps (oculus, instagram, facebook, messenger, whastapp, threads, etc)
+- How:
+    - Flexible schema leveraging the map datatype: No ALTER TABLE, add key to map when an extra column is needed (~65k limit)
+- Graph Modeling:
+    - Usually the model takes 2 Nodes and links them through an edge, this looks like:
+        - subject_identifier: STRING
+        - subject_type: VERTEX_TYPE
+        - object_identifier: STRING
+        - object_type: VERTEX_TYPE
+        - edge_type: EDGE_TYPE
+        - properties: MAP<STRING,STRING>
+    - Example: "Player plays on team"
+        - subject_identifier: player_name
+        - subject_type: player
+        - object_identifier: team_name
+        - object_type: team
+        - edge_type: PLAYS_ON
+        - properties: How many years did they play on that team, when did they start, etc.
+- Graph example: [ChicagoBulls]<--Plays_ON-->[MichaelJordan]<--Plays_AGAINST-->[JohnStockton]<--Plays_ON-->[UtahJazz]
 */
