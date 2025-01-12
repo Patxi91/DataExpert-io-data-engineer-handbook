7e6d4e9d18e4e050d8eb664e0f177fd1d4f3c2f7
Patxi Oyaga <oyaga888@gmail.com>
2025-01-12 18:17:00 +0100

    update comment

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
@@ -3,7 +3,7 @@
 -------------------------------------------------
 /*
 Enumerations and Subpartitions pattern usecases:
-- Whenever you have tons of sources mapping to a shared schema
+- Whenever you have tons of sources mapping to a shared schema/logic/output
     - Unit Economics (fees, coupons, credits, insurance, infrastructure cost, taxes, etc)
     - Infrastructure Graph (applications, DBs, servers, code bases, CI/CD jobs, etc)
     - Family of Apps (oculus, instagram, facebook, messenger, whastapp, threads, etc)
