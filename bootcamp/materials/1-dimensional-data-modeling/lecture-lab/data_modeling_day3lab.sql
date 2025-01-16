8f1be3617e99df5b48f86b52a8a7f71b3982aef2
Patxi Oyaga <oyaga888@gmail.com>
2025-01-16 21:23:33 +0100

    game, player vertices

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
@@ -27,7 +27,7 @@ Enumerations and Subpartitions pattern usecases:
 - Graph example: [ChicagoBulls]<--Plays_ON-->[MichaelJordan]<--Plays_AGAINST-->[JohnStockton]<--Plays_ON-->[UtahJazz]
 */
 
--- Vertices and Edges
+-- Vertices andd Edges
 DO $$
 BEGIN
     -- Check and drop vertex_type if it exists
@@ -76,3 +76,45 @@ CREATE TABLE edges(
 					object_type,
 					edge_type)
 );
+
+-- Game
+-- Let's think of game as a vertex
+INSERT INTO vertices
+SELECT
+	game_id AS identifier,
+	'game'::vertex_type AS type,
+	json_build_object(
+		'pts_home', pts_home,
+		'pts_away', pts_away,
+		'winning_team', CASE WHEN home_team_wins = 1 THEN home_team_id ELSE visitor_team_id END
+	) as properties
+FROM games;
+
+-- Player
+-- Player details will be an aggregated view from table game_details since we have player_name available
+INSERT INTO vertices
+WITH players_agg AS(
+	SELECT
+		player_id AS identifier,
+		MAX(player_name) AS player_name,
+		COUNT(1) as number_of_games,
+		SUM(pts) as total_points,
+		ARRAY_AGG(DISTINCT team_id) AS teams
+	FROM game_details
+	GROUP BY player_id
+)
+SELECT 
+	identifier,
+	'player'::vertex_type,
+	json_build_object(
+		'player_name', player_name,
+		'number_of_games', number_of_games,
+		'total_points', total_points,
+		'teams', teams
+		)
+FROM players_agg
+
+-- Team
+
+
+
