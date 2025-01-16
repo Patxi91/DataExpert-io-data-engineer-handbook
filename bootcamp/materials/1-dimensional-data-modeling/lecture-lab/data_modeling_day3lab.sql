1011f4115f9ce29c4e144cc060502f1baa2456db
Patxi Oyaga <oyaga888@gmail.com>
2025-01-17 00:02:49 +0100

    team vertix

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/data_modeling_day3lab.sql
@@ -115,6 +115,20 @@ SELECT
 FROM players_agg
 
 -- Team
-
-
-
+INSERT INTO vertices
+WITH teams_deduped AS( -- dedupe as for some reason data was triplicated
+	SELECT *, ROW_NUMBER() OVER(PARTITION BY team_id) as row_num
+	FROM teams
+)
+SELECT
+	team_id AS identifier,
+	'team'::vertex_type AS type,
+	json_build_object(
+		'abbreviation', abbreviation,
+		'nickname', nickname,
+		'city', city,
+		'arena', arena,
+		'year_founded', yearfounded
+		)
+FROM teams_deduped
+WHERE row_num = 1;
