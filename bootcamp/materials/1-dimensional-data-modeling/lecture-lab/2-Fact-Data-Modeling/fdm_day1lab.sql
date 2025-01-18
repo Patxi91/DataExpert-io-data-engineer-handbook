5362c56daf26a9edf9c847bd14fb6725fc18acb5
Patxi Oyaga <oyaga888@gmail.com>
2025-01-18 18:48:00 +0100

    notes

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/2-Fact-Data-Modeling/fdm_day1lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/2-Fact-Data-Modeling/fdm_day1lab.sql
@@ -2,7 +2,18 @@
 -----      LAB 1: Fact Data Modeling       -----
 ------------------------------------------------
 /*
-NOTES
+Fact Modeling: Who, What, Where, When and How:
+    - Who: IDs (Car ID, User ID, OS ID, Phone ID)
+    - Where: Geographical(country), Where in the app (Homepage, Profile page): IDs, /where or /profile, etc.
+    - How: Similar to where/how --> "He used an iphone to make this click" --> (How: iphone, Where: Homepage)
+    - What: The atomic event that happened, a notification was "generated", "sent", "clicked", "delivered"
+    - When: "event_timestamp", "event_date" (Client-side logging, ideally UTC)
+Options when working with high-volume data:
+    - Sampling: metric-driven use-cases
+    - Bucketing:
+        - Fact data by an important dimension (usually user)
+        - Bucket joins > Shuffle joins
+        - Sorted-merge-Bucket (SMB) joins can do joins without Shuffle at all!
 */
 
 ---------
