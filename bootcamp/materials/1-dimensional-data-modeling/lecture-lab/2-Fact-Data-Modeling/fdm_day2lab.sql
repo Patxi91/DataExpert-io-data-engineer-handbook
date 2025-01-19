5c7242d6ccccfe79d1d5c1981f410dd57507d6db
Patxi Oyaga <oyaga888@gmail.com>
2025-01-19 14:57:23 +0100

    fdm day2

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/2-Fact-Data-Modeling/fdm_day2lab.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/2-Fact-Data-Modeling/fdm_day2lab.sql
@@ -28,14 +28,12 @@ Datelist Ints:
 -----------------------------------
 ----     Lab 2 - DateList      ----
 -----------------------------------
--- week 2 lab 2
 
-create table users_cumulated (
+-- Cumulative users table build from events
+create table if not exists users_cumulated (
     user_id text,
-    -- lists of dates in the past where the user was active
-    dates_active date[],
-    -- current date for the user
-    date date,
+    dates_active date[], -- The list of dates in the past where the user was active
+    date date, -- Current date for the user
     primary key (user_id, date)
 );
 
@@ -43,14 +41,14 @@ insert into users_cumulated
 with yesterday as (
     select *
     from users_cumulated
-    where date = date('2023-01-30')
+    where date = date('2023-01-30') -- yesterday
 ), today as (
     select
         cast(user_id as text) as user_id,
         date(cast(event_time as timestamp)) as date_active
     from events
     where
-        date(cast(event_time as timestamp)) = date('2023-01-31')
+        date(cast(event_time as timestamp)) = date('2023-01-31') -- today
         and user_id is not null -- deal with null user_ids in this data
     group by user_id, date(cast(event_time as timestamp))
 )
@@ -67,8 +65,8 @@ select
 from today t full outer join yesterday y
 on t.user_id = y.user_id;
 
-
--- generate a datelist for 30 days
+-- Turn into a DateList: Bits where recent data is 1st or left and oldest data is the last bit or right one
+-- generate a datelist for 30 days    
 with users as (
     select * from users_cumulated
     where date = date('2023-01-31')
@@ -95,10 +93,10 @@ select
     -- bit_count() can give us how many times the user is active
     bit_count(cast(cast(sum(placeholder_int_value) as bigint) as bit(32))) > 0 as dim_is_monthly_active,
     -- let's check a user is active in the last 7 days
-    bit_count(cast('11111110000000000000000000000000' as bit(32)) & --bit-wise and
+    bit_count(cast('11111110000000000000000000000000' as bit(32)) & --bit-wise AND for a week
         cast(cast(sum(placeholder_int_value) as bigint) as bit(32))) > 0 as dim_is_weekly_active,
     -- daily is the same but with the 1st one only 1
-    bit_count(cast('10000000000000000000000000000000' as bit(32)) & --bit-wise and
+    bit_count(cast('10000000000000000000000000000000' as bit(32)) & --bit-wise AND for a day
         cast(cast(sum(placeholder_int_value) as bigint) as bit(32))) > 0 as dim_is_daily_active
 from place_holder_ints
 group by user_id;
